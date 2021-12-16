7f71e6a378b8a14f9558cf2c553ea4cb144a9710
danyalutsevich <luchevich31@gmail.com>
2021-12-17 00:21:05 +0200

    icon time name

--- Client/resource.h
+++ Client/resource.h
@@ -2,7 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by Client.rc
 //
-#define IDB_PNG1                        101
+
 #define IDI_ICON1                       102
 
 // Next default values for new objects
